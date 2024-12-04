f4106b1af53725c0cfef043e5462add5a5613e0f
sina <153110415+sinajannesar@users.noreply.github.com>
2024-12-04 10:23:56 +0330

    The third part
    Add the parsePDA(filePath) function. This function is for reading the input file and converting it to PDA, which includes reading the file, discarding extra lines, reading the final states, processing the transfer, creating the machine, and some small changes for readability and simplifying the code.

--- PDA.js
+++ PDA.js
@@ -1,11 +1,11 @@
 const fs = require("fs")
+const path = require("path");
 
 class PDA {
     constructor(transitions, finalStates) {
         this.transitions = transitions;
-        this.finalStates = new setImmediate(finalStates);
-        this.currentState = 0;
-        this.stack = [];
+        this.finalStates = new Set(finalStates);
+        this.reset();
     }
 
     reset() {
@@ -20,7 +20,7 @@ class PDA {
                 (t) =>
                     t.from === this.currentState &&
                     t.symbol === char &&
-                    (t.pop === "#" || this.stack[this.stack.length - 1] === t.pop)
+                    (t.pop === "#" || this.stack.at(-1) === t.pop)
             );
 
             if (!transition) return false;
@@ -32,4 +32,17 @@ class PDA {
 
         return this.finalStates.has(this.currentState);
     }
+    }
+
+
+    function parsePDA(filePath) {
+        const [alphabetCount, , ...lines] = fs.readFileSync(filePath, "utf8").trim().split("\n");
+        const finalStates = lines[alphabetCount].split(" ").map(Number);
+        const transitions = lines.slice(alphabetCount + 2).map((line) => {
+            const [from, symbol, to, pop, push] = line.split(" ");
+            return { from: +from, symbol, to: +to, pop, push };
+          });
+
+          return new PDA(transitions, finalStates);
 }
+
